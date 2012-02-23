c99d2e4ea270f4eb128ce27e962932c045fb4b42
Ryan Chavez <ryan22chavez@gmail.com>
2012-02-23 03:45:41 -0800

    Holy moly!

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,8 +2,20 @@ $(document).ready(function() {
 
 
   $('.fancybox').fancybox();
+  $('.dropdown-toggle').dropdown()
+  $('.carousel').carousel() {
+  interval: 2000
+  }
+  $('#example').tooltip(options)
   
 
-
+  
+  $(".collapse").collapse();
+  $(".collapse").collapse();
+  $(".collapse").collapse();
+  $(".collapse").collapse();
+  $(".collapse").collapse();
+  
+  
 });
 
